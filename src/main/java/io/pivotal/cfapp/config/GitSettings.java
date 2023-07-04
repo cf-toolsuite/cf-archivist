dad335d3c0670f105b4c69ce4b3ca432b03da0f9
Chris Phillipson <cphillipson@pivotal.io>
2023-07-04 13:27:30 -0700

    Make GitSettings mutable
    * it was causing startup issues in its older immutable form

--- src/main/java/io/pivotal/cfapp/config/GitSettings.java
+++ src/main/java/io/pivotal/cfapp/config/GitSettings.java
@@ -4,23 +4,17 @@ import java.util.Set;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.bind.ConstructorBinding;
 import org.springframework.context.annotation.Configuration;
 
-import lombok.Builder;
-import lombok.Builder.Default;
-import lombok.Getter;
+import lombok.Data;
 
-@Builder
-@Getter
+@Data
 @Configuration
 @ConfigurationProperties(prefix = "cf.policies.git")
 public class GitSettings {
 
-    @Default
     private String uri = "";
     private String username;
-    @Default
     private String password = "";
     private String commit;
     private Set<String> filePaths;
@@ -37,17 +31,4 @@ public class GitSettings {
         return StringUtils.isNotBlank(uri);
     }
 
-    @ConstructorBinding
-    GitSettings(
-        String uri,
-        String username,
-        String password,
-        String commit,
-        Set<String> filePaths) {
-            this.uri = uri;
-            this.username = username;
-            this.password = password;
-            this.commit = commit;
-            this.filePaths = filePaths;
-    }
 }
