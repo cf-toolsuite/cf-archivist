a49eeb6e2dcd322385629780f09e6aa206075f62
Chris Phillipson <cphillipson@pivotal.io>
2024-03-12 20:11:04 -0700

    Baseline on Java 21

---
+++ src/test/java/io/pivotal/cfapp/CfArchivistApplicationTests.java
@@ -0,0 +1,13 @@
+package io.pivotal.cfapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CfArchivistApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
