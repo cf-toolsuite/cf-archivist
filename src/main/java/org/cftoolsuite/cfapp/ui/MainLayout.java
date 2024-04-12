c7bb5f827e08aaa683b6940e3173d621703a3bc5
Chris Phillipson <cphillipson@pivotal.io>
2024-04-11 18:57:25 -0700

    Make more secure; only open public-access to certain paths. And enhance navigation

--- src/main/java/org/cftoolsuite/cfapp/ui/MainLayout.java
+++ src/main/java/org/cftoolsuite/cfapp/ui/MainLayout.java
@@ -1,19 +1,21 @@
 package org.cftoolsuite.cfapp.ui;
 
+import org.cftoolsuite.cfapp.ui.view.HomeView;
+import org.cftoolsuite.cfapp.ui.view.SnapshotApplicationDetailView;
+import org.cftoolsuite.cfapp.ui.view.SnapshotServiceInstanceDetailView;
+
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.accordion.Accordion;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
 import com.vaadin.flow.component.details.DetailsVariant;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.tabs.Tab;
 import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.router.RouterLink;
-
-import org.cftoolsuite.cfapp.ui.view.HomeView;
-import org.cftoolsuite.cfapp.ui.view.SnapshotApplicationDetailView;
-import org.cftoolsuite.cfapp.ui.view.SnapshotServiceInstanceDetailView;
 
 
 public class MainLayout extends AppLayout {
@@ -45,10 +47,19 @@ public class MainLayout extends AppLayout {
     }
 
     private Tab createTab(Icon icon, String label, Class<? extends Component> layout) {
-    	RouterLink link = new RouterLink(label, layout);
-    	Tab tab = new Tab();
-    	tab.add(icon, link);
-    	return tab;
+		RouterLink link = new RouterLink();
+    	link.setRoute(layout);
+		Div container = new Div();
+		container.getStyle().set("display", "flex");
+		container.getStyle().set("justify-content", "flex-start");
+		container.getStyle().set("align-items", "center");
+		container.getStyle().set("width", "100%"); // Ensure it takes up the full width
+		icon.getStyle().set("margin-right", "8px"); // Adjust the space as needed
+		container.add(icon, new Span(label));
+		link.add(container);
+		Tab tab = new Tab();
+		tab.add(link);
+		return tab;
     }
 
 }
